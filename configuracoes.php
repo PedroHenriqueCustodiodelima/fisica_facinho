7b30852bf344a2e9818b7a80526ec18562d31843
thiagormoura <thiago.joao@Escolar.ifrn.edu.br>
2024-11-11 11:31:38 -0300

    melhorias front

--- configuracoes.php
+++ configuracoes.php
@@ -1,6 +1,29 @@
-<!-- incluindo todas as funções da pagina de funções -->
 <?php
+
 include("funcoes_php/funcoes_confi.php");
+
+
+if (!isset($usuario)) {
+    
+    header('Location: login.php');
+    exit();
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['imagem'])) {
+    $imagem = $_FILES['imagem'];
+    $destino = 'caminho_para_salvar_imagem/' . basename($imagem['name']);
+    
+    // Verificação do tipo e tamanho do arquivo
+    if ($imagem['size'] <= 5000000 && in_array($imagem['type'], ['image/jpeg', 'image/png', 'image/gif'])) {
+        if (move_uploaded_file($imagem['tmp_name'], $destino)) {
+        
+        }
+    } else {
+        // Exibe uma mensagem de erro caso o arquivo não seja válido
+        echo "Arquivo inválido ou muito grande.";
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -11,10 +34,9 @@ include("funcoes_php/funcoes_confi.php");
   <title>Configurações</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
   <link rel="stylesheet" href="css/configuracoes.css">
 </head>
-<body>
+<body class="d-flex flex-column" style="min-height: 100vh;"> <!-- Define a altura mínima da página -->
 
   <header class="d-flex justify-content-between align-items-center">
     <a href="inicio.php">
@@ -26,140 +48,66 @@ include("funcoes_php/funcoes_confi.php");
     </div>
   </header>
 
-  <div class="container">
-  <div class="voltar-container mb-4">
-        <a href="inicio.php" class="custom-link">
-            <i class="fa-solid fa-circle-arrow-left"></i> <span>Voltar</span>
-        </a>
-    </div>
-    <main>
-      <div class="container-opcao">
-        <div class="perfil">
-          <img id="avatar-imagem-main" src="<?php echo htmlspecialchars($usuario['foto']); ?>" alt="Avatar">
-          <p>
-            <span id="usuario-nome-main" class="usuario-nome-main"><?php echo htmlspecialchars($usuario['nome']); ?></span>
-            <span class="data-criacao">Conosco desde <?php echo htmlspecialchars($usuario['data_criacao_formatada']); ?></span>
-          </p>
-        </div>
-        <button type="button" class="editar" data-toggle="modal" data-target="#editarPerfilModal">
-          <i class="fa-solid fa-pen" style="width: 24px; height: 24px; color: #ffffff;"></i>
-        </button>
-        <div class="caixas">
-          <div class="caixa caixa-esquerda">
-            <canvas id="grafico-esquerda"></canvas>
-          </div>
-          <div class="caixas-direita">
-            <div class="caixa caixa-direita-superior">
-              <div class="texto-centralizado">
-                <span class="numero"><?php echo $contagemRespostas['corretas']; ?></span>
-                <span class="descricao">Acertos</span>
-              </div>
-            </div>
-            <div class="caixa caixa-direita-inferior">
-              <div class="texto-centralizado">
-                <span class="numero_1"><?php echo $contagemRespostas['erradas']; ?></span>
-                <span class="descricao_1">Erradas</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
+  <div class="col-8 voltar-container " style="margin: 20px;">
+    <a href="inicio.php" class="custom-link" style="text-decoration: none;color: #001A4E; ">
+        <i class="fa-solid fa-circle-arrow-left"></i> <span>Voltar</span>
+    </a>
   </div>
-  
-<script>
-document.addEventListener('DOMContentLoaded', function () {
-    var ctxEsquerda = document.getElementById('grafico-esquerda').getContext('2d');
-    var graficoEsquerda = new Chart(ctxEsquerda, {
-        type: 'pie', 
-        data: {
-            labels: ['Acertos', 'Erradas'], 
-            datasets: [{
-                label: 'Respostas',
-                data: [
-                    <?php echo $contagemRespostas['corretas']; ?>,
-                    <?php echo $contagemRespostas['erradas']; ?>
-                ], 
-                backgroundColor: [
-                    'rgba(75, 192, 192, 0.2)', 
-                    'rgba(255, 99, 132, 0.2)'  
-                ], 
-                borderColor: [
-                    'rgba(75, 192, 192, 1)', 
-                    'rgba(255, 99, 132, 1)' 
-                ], 
-                borderWidth: 1
-            }]
-        },
-        options: {
-            responsive: true,
-            plugins: {
-                legend: {
-                    position: 'top', 
-                },
-                tooltip: {
-                    callbacks: {
-                        label: function (tooltipItem) {
-                            return tooltipItem.label + ': ' + tooltipItem.raw;
-                        }
-                    }
-                },
-                datalabels: {
-                    display: false 
-                }
-            }
-        }
-    });
-});
-</script>
 
-<div class="modal fade" id="editarPerfilModal" tabindex="-1" role="dialog" aria-labelledby="editarPerfilModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <div class="modal-header-left">
-          <img src="img/logo.png" alt="Logo" class="modal-logo">
-          <h5 class="modal-title" id="editarPerfilModalLabel">Editar Perfil</h5>
+  <main class="d-flex justify-content-center align-items-center ">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 container-opcao">
+          <div class="col-12 perfil">
+            <!-- Imagem do perfil com função de troca ao clicar -->
+            <form id="troca-imagem-form" action="" method="post" enctype="multipart/form-data">
+              <img id="preview-imagem" src="<?php echo htmlspecialchars($usuario['foto']); ?>" alt="Imagem" class="img-thumbnail" onclick="document.getElementById('upload-imagem').click()">
+              <input type="file" id="upload-imagem" name="imagem" accept="image/*" style="display: none;" onchange="alterarImagemPerfil()">
+            </form>
+            <p>
+              <span id="usuario-nome-main" class="usuario-nome-main"><?php echo htmlspecialchars($usuario['nome']); ?></span>
+              <span class="data-criacao">Conosco desde <?php echo htmlspecialchars($usuario['data_criacao_formatada']); ?></span>
+            </p>
+          </div>
         </div>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <form action="" method="post" enctype="multipart/form-data">
-          <div class="form-group">
-            <label for="upload-imagem">Imagem de Perfil</label>
-            <div class="imagem-perfil-container">
-              <img id="preview-imagem" src="<?php echo htmlspecialchars($usuario['foto']); ?>" alt="Imagem" class="img-thumbnail">
-              <input type="file" id="upload-imagem" name="imagem" accept="image/*" class="form-control-file mt-2" style="display: none;">
+
+        <!-- Formulário de edição de perfil -->
+        <div class="col-12 editar-perfil-container mt-4">
+          <form action="" method="post" enctype="multipart/form-data">
+            <div class="form-group">
+              <label for="nome" style=" color: #001A4E;">Nome</label>
+              <input type="text" class="form-control" id="nome" name="nome" value="<?php echo htmlspecialchars($usuario['nome']); ?>">
             </div>
-          </div>
-          <div class="form-group">
-            <label for="nome">Nome</label>
-            <input type="text" class="form-control" id="nome" name="nome" value="<?php echo htmlspecialchars($usuario['nome']); ?>">
-          </div>
-          <div class="form-group">
-            <label for="email">E-mail</label>
-            <input type="email" class="form-control" id="email" name="email" value="<?php echo htmlspecialchars($usuario['email']); ?>">
-          </div>
-          <button type="submit" class="btn btn-primary">Salvar</button>
-        </form>
+            <div class="form-group">
+              <label for="email" style=" color: #001A4E;">E-mail</label>
+              <input type="email" class="form-control" id="email" name="email" value="<?php echo htmlspecialchars($usuario['email']); ?>">
+            </div>
+            <div class="form-group">
+              <label for="senha" style=" color: #001A4E;">Senha</label>
+              <input type="senha" class="form-control" id="senha" name="senha" value="<?php echo htmlspecialchars($usuario['']); ?>">
+            </div>
+            <button type="submit" class="btn " style="background-color: #001A4E; color:white;">Salvar</button>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-</div>
+  </main>
 
-<footer>
+  <footer class="mt-auto text-white text-center py-3" style="background-color: #001A4E;">
   <p>Copyright © 2023 | Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Norte</p>
 </footer>
 
-<script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-3d"></script>
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script src="js/confi.js"></script>
-<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+  <script>
+    function alterarImagemPerfil() {
+      // Submete o formulário automaticamente ao selecionar uma nova imagem
+      document.getElementById('troca-imagem-form').submit();
+    }
+  </script>
 
 </body>
 </html>
