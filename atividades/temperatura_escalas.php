84119611bb1aa4384604b22019b78b31c00ac086
Pedro Henrique Custodio de Lima <lima.custodio@escolar.ifrn.edu.br>
2025-01-07 18:03:22 -0300

    todas as paginas de atividades completas

--- atividades/temperatura_escalas.php
+++ atividades/temperatura_escalas.php
@@ -53,74 +53,14 @@ $pagina_atual = isset($_GET['pagina']) ? intval($_GET['pagina']) : 1;
 $pagina_atual = ($pagina_atual > 0) ? $pagina_atual : 1;
 $offset = ($pagina_atual - 1) * $questoes_por_pagina;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-$total_questoes_sql = "SELECT COUNT(*) AS total FROM $tabela WHERE materia = 'grandezas'";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$total_questoes_sql = "SELECT COUNT(*) AS total FROM $tabela WHERE materia = 'Temperatura e escalas de medida'";
 
 $total_questoes_result = $conn->query($total_questoes_sql);
 $total_questoes_row = $total_questoes_result->fetch_assoc();
 $total_questoes = $total_questoes_row['total'];
 $total_paginas = ceil($total_questoes / $questoes_por_pagina);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-$questao_sql = "SELECT id, enunciado, resolucao, foto_enunciado, materia, ano, dificuldade FROM $tabela WHERE materia = 'grandezas' LIMIT $questoes_por_pagina OFFSET $offset";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$questao_sql = "SELECT id, enunciado, resolucao, foto_enunciado, materia, ano, dificuldade FROM $tabela WHERE materia = 'Temperatura e escalas de medida' LIMIT $questoes_por_pagina OFFSET $offset";
 
 $questao_result = $conn->query($questao_sql);
 $questoes_data = [];
@@ -197,38 +137,7 @@ $pagina_atual = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
 $questoes_por_pagina = 1;
 $offset = ($pagina_atual - 1) * $questoes_por_pagina;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-$count_sql = "SELECT COUNT(*) AS total FROM $tabela WHERE materia = 'grandezas'";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$count_sql = "SELECT COUNT(*) AS total FROM $tabela WHERE materia = 'Temperatura e escalas de medida'";
 
 $conditions = [];
 if ($enunciadoFiltro) {
@@ -250,30 +159,7 @@ $total_questoes = $count_result->fetch_assoc()['total'];
 $total_paginas = ceil($total_questoes / $questoes_por_pagina);
 
 $questao_sql = "SELECT id, enunciado, resolucao, foto_enunciado, materia, ano, dificuldade
-
-
-
-
-
-
-
-
-
-
-
-                FROM $tabela WHERE materia = 'grandezas'";
-
-
-
-
-
-
-
-
-
-
-
-
+    FROM $tabela WHERE materia = 'Temperatura e escalas de medida'";
 
 if (count($conditions) > 0) {
     $questao_sql .= " AND " . implode(" AND ", $conditions);
@@ -327,7 +213,7 @@ while ($questao = $questao_result->fetch_assoc()) {
             <a href="../assunto_p1.php" class="btn btn-voltar d-flex align-items-center">
                 <i class="fa-solid fa-circle-arrow-left"></i> Voltar
             </a>
-            <h3 class="font-weight-bold">Introdução a Física</h3>
+            <h3 class="font-weight-bold">Temperatura e escalas de medida</h3>
         </div>
 
         <div class="filters-container mb-4">
